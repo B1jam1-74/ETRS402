d24787348964538b84ab5d75b777de36db925ebc
B1jam1-74 <bavocatmaulaz@gmail.com>
2025-03-31 08:23:20 +0200

    ajout script arduino

--- rotation_servo_moteur/rotation_servo_moteur.ino
+++ rotation_servo_moteur/rotation_servo_moteur.ino
@@ -3,24 +3,25 @@
 Servo myServo;      // Créer un objet Servo pour contrôler le servomoteur
 
 int pt_depart = 0;
-int pt_arrive = 270;
+int pt_arrive = 180;
 
 void setup() {
   myServo.attach(3);  // Attacher le servomoteur à la broche 3
-  serial.begin(9600);
+  Serial.begin(9600);
 }
 
 void loop() {
   // Faire tourner le servomoteur de 0 à 270 degrés (gauche à droite)
   for (int angle = pt_depart; angle <= pt_arrive; angle++) {
     myServo.write(angle);  // Définir l'angle du servomoteur
-    delay(50);             // Attendre 50 ms pour que le servomoteur atteigne la position
-    serial.println(angle);
+    delay(15);             // Attendre 50 ms pour que le servomoteur atteigne la position
+    Serial.println(angle);
   }
 
   // Faire tourner le servomoteur de 270 à 0 degrés (droite à gauche)
   for (int angle = pt_arrive; angle >= pt_depart; angle--) {
     myServo.write(angle);  // Définir l'angle du servomoteur
-    delay(50);             // Attendre 50 ms pour que le servomoteur atteigne la position
+    delay(15);             // Attendre 50 ms pour que le servomoteur atteigne la position
+    Serial.println(angle);
   }
 }
